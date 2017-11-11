a95241e138083150b20bee25754798b7a96eb25e
Alan-Miller <alan.ashman@gmail.com>
2017-11-11 03:43:51 -0700

    v2

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="box one">
-          <span className="text one">TEXT ONE</span>
-          <span className="text two">TEXT TWO</span>
-          <span className="text thr">TEXT THR</span>
-        </div>
-        <div className="box two">
-          <span className="text one">TEXT ONE</span>
-          <span className="text two">TEXT TWO</span>
-          <span className="text thr">TEXT THR</span>
-        </div>
-        <div className="box thr">
-          <span className="text one">TEXT ONE</span>
-          <span className="text two">TEXT TWO</span>
-          <span className="text thr">TEXT THR</span>
+        <div className="parallax-container">
+          <div className="top foreground layer">Top Foreground</div>
+          <div className="top midground layer">Top Midground</div>
+          <div className="top background layer">Top Background</div>
+
+          <div className="middle foreground layer">Middle Foreground</div>
+          <div className="middle midground layer">Middle Midground</div>
+          <div className="middle background layer">Middle Background</div>
+
+          <div className="bottom foreground layer">Bottom Foreground</div>
+          <div className="bottom midground layer">Bottom Midground</div>
+          <div className="bottom background layer">Bottom Background</div>
         </div>
       </div>
     );
